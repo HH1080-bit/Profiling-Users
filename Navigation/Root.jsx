1c573ec7ceebdbe9614352f9c1d8fa2b315647eb
HH1080-bit <71850199+HH1080-bit@users.noreply.github.com>
2023-06-05 20:23:16 +0300

    Add files via upload

---
+++ Navigation/Root.jsx
@@ -0,0 +1,37 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import Home from '../Screens/home/Home';
+import User from '../Screens/profile/User';
+
+const Stack = createNativeStackNavigator();
+
+const Root = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="home" component={Home} options={{
+                title: 'Home',
+                headerStyle: {
+                    backgroundColor: '#3187f9',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+                }}
+            />
+            <Stack.Screen name="userdetails" component={User} options={{
+                title: 'User Details',
+                headerStyle: {
+                    backgroundColor: '#3187f9',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+                }}
+        />
+        </Stack.Navigator>
+    );
+}
+
+export default Root;
---
+++ Screens/home/Search.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import {View, StyleSheet, TextInput} from 'react-native';
+
+const Search = ({getSearchWord}) => {
+    const [searchText, setsearchText] = useState('');
+
+    const handleChangeText = (val) => {
+        setsearchText(val)
+        getSearchWord(val)
+    }
+    return (
+        <View>
+            <TextInput style={styles.searchInput} value={searchText} onChangeText={handleChangeText} placeholder='Search by name'></TextInput>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    searchInput: {
+        backgroundColor: 'white',
+        padding: 8,
+        color:"#3187f9",
+        fontSize:20,
+        margin: 12,
+        borderRadius: 2,
+        borderBottomColor:"#3187f9",
+        borderBottomWidth:2
+    }
+})
+// FE5655
+export default Search;
